eaba6b403782b62f76773be862b0c6d7ae3fc177
Giovanni Di Martino <Giovanni.DiMartino@nttdata.com>
2022-02-02 11:16:36 +0100

    use flask-mqtt

--- microservice_mqtt_publisher/src/main/AppMqttPublisher.py
+++ microservice_mqtt_publisher/src/main/AppMqttPublisher.py
@@ -2,19 +2,26 @@ from flask import Flask
 from flask_cors import CORS
 from flask import request
 from configuration.config import read_config
-from controller.MQTTPublisher import Publisher
 from controller.RabbitMqClient import RabbitMQ
 import random
 import string
 import json
-
-app = Flask(__name__)
-CORS(app)
+from flask_mqtt import Mqtt
 
 config = read_config()
 client_id = 'device_endpoint'.join(random.choices(string.ascii_letters + string.digits, k=8))
-publisher = Publisher(client_id, config.get("MQTT", "broker"), config.get("MQTT", "port"))
-publisher.start_connection()
+
+app = Flask(__name__)
+app.config['MQTT_CLIENT_ID'] = client_id
+app.config['MQTT_BROKER_URL'] = config.get("MQTT", "broker")  # use the free broker from HIVEMQ
+app.config['MQTT_BROKER_PORT'] = int(config.get("MQTT", "port"))  # default port for non-tls connection
+app.config['MQTT_USERNAME'] = ''  # set the username here if you need authentication for the broker
+app.config['MQTT_PASSWORD'] = ''  # set the password here if the broker demands authentication
+app.config['MQTT_KEEPALIVE'] = 5  # set the time interval for sending a ping to the broker to 5 seconds
+app.config['MQTT_TLS_ENABLED'] = False  # set TLS to disabled for testing purposes
+CORS(app)
+
+mqtt = Mqtt(app)
 
 rabbitmq = RabbitMQ("device_endpoint", config)
 rabbitmq.start_connection()
@@ -25,7 +32,7 @@ def publish_mqtt(actuator, device_id):
     topic = actuator + "/" + device_id
     payload = request.get_json(force=True)
     msg = payload["message"]
-    publisher.publish(topic, msg)
+    mqtt.publish(topic, msg)
     return "true"
 
 
